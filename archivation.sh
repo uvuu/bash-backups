03a2f6bb9c7599776c2c003c5ee3571b1fad71c8
root <root@thewayfarer.novalocal>
2020-07-07 06:17:30 +0000

    Added archivation option all-in-one(-A)

--- archivation.sh
+++ archivation.sh
@@ -2,43 +2,56 @@
 
 DIR='~/backups/archives' # Directory where we store the packed files
 if [ -n "$1" ]; then
-DIR=$1
+  DIR=$1
 fi
 
 TIME=''
 if [ -n "$2" ]; then
-TIME=$2
+  TIME=$2
 fi
 
 # Put here separate files for archiving(archives will have prefix F_) 
 FILES=(
- "/etc/apache2/sites-available/thewayfarer.xyz.conf"
+  "/etc/apache2/sites-available/thewayfarer.xyz.conf"
 )
 
 # Put directories here for non-recursive archiving(archives will have prefix NR_)
 NON_RECURSIVE=(
- "/root/backups" # Pack backuping stuff itself
- "/root/backups/dumps"
+  "/root/backups" # Pack backuping stuff itself
+  "/root/backups/dumps"
 )
 
 # Put directories here for recursive archiving(archives will have prefix R_)
 RECURSIVE=(
- "/var/www/html"
+  "/var/www/html"
 )
 
 ARCHIVE=''
+if [ "$3" = "-A" ]; then
+  ARCHIVE='all_in_one'
 
-for path in ${FILES[@]}; do
-  ARCHIVE="F$(realpath $path | tr / _)"
-  tar cvfz "$DIR/$ARCHIVE.$TIME.tar.gz" $path
-done
+  NON_RECURSIVE_FILES=()
+  for path in ${NON_RECURSIVE[@]}; do
+    while IFS= read -d $'\0' -r file ; do
+       NON_RECURSIVE_FILES=("${NON_RECURSIVE_FILES[@]}" "$file")
+    done < <(find "$path" -maxdepth 1 -type f -print0)
+  done
 
-for path in ${NON_RECURSIVE[@]}; do
-  ARCHIVE="NR$(realpath $path | tr / _)"
-  find $path -maxdepth 1 -type f -exec tar cvfz "$DIR/$ARCHIVE.$TIME.tar.gz" {} +
-done
+  ALL_PATHS=("${FILES[@]}" "${NON_RECURSIVE_FILES[@]}" "${RECURSIVE[@]}")
+  tar cvfz "$DIR/$ARCHIVE.$TIME.tar.gz"  "${ALL_PATHS[@]}"
+else
+  for path in ${FILES[@]}; do
+    ARCHIVE="F$(realpath $path | tr / _)"
+    tar cvfz "$DIR/$ARCHIVE.$TIME.tar.gz" $path
+  done
 
-for path in ${RECURSIVE[@]}; do
-  ARCHIVE="R$(realpath $path | tr / _)"
-  tar cvfz "$DIR/$ARCHIVE.$TIME.tar.gz" $path
-done
+  for path in ${NON_RECURSIVE[@]}; do
+    ARCHIVE="NR$(realpath $path | tr / _)"
+    find $path -maxdepth 1 -type f -exec tar cvfz "$DIR/$ARCHIVE.$TIME.tar.gz" {} +
+  done
+
+  for path in ${RECURSIVE[@]}; do
+    ARCHIVE="R$(realpath $path | tr / _)"
+    tar cvfz "$DIR/$ARCHIVE.$TIME.tar.gz" $path
+  done
+fi
